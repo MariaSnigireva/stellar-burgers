add4eb8b3ec26fb21b0e4ced903fff39b1ab997c
Maria Snigireva <Masch.dark@yandex.ru>
2024-12-13 20:36:47 +0300

    begin

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,18 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import React from 'react';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { Feed } from '../../pages/feed';
+import { Login } from '../../pages/login';
+import { Register } from '../../pages/register';
+import { ForgotPassword } from '../../pages/forgot-password';
+import { ResetPassword } from '../../pages/reset-password';
+import { Profile } from '../../pages/profile';
+import { ProfileOrders } from '../../pages/profile-orders';
+import { NotFound404 } from '../../pages/not-fount-404';
+import { Modal } from '../modal';
+import { IngredientDetails } from '../ingredient-details';
 
 const App = () => (
   <div className={styles.app}>
